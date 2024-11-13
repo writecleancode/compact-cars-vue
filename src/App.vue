9ddb41fc5bb405b953c350fdc8e6735c6d5637e1
Rafal <rafek131@wp.pl>
2024-11-13 21:38:19 +0100

    added cars data; basic check if cars data is displayed

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
+<script>
+import { cars as carsData } from '/src/data/cars.ts';
+
+export default {
+	setup() {
+		const cars = carsData;
+
+		return { cars };
+	},
+};
+</script>
+
 <template>
 	<h1>Segment C Hatchback cars <span>(late 1990's, early 2000's)</span></h1>
+	<div>
+		<div v-for="car in cars" :key="car.brand">{{ car.brand }} {{ car.model }}</div>
+	</div>
 </template>
