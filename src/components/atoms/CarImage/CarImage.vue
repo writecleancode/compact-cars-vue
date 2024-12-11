29ee716cca7f606356de8c8cc085a780b5762060
Rafal <rafek131@wp.pl>
2024-12-11 21:09:18 +0100

    further paths fixes

--- src/components/atoms/CarImage/CarImage.vue
+++ src/components/atoms/CarImage/CarImage.vue
@@ -4,14 +4,24 @@ export default {
 		imgUrl: Object,
 		altText: String,
 	},
+
+	setup() {
+		const basePath = import.meta.env.VITE_BASE_PATH;
+
+		return {
+			basePath,
+		};
+	},
 };
 </script>
 
 <template>
 	<a v-bind:href="imgUrl?.big || imgUrl?.medium || undefined" target="_blank" class="car-image-wrapper-link">
 		<img
-			v-bind:srcset="`${imgUrl?.small || '/assets/img/unknown_car.jpg'}, ${imgUrl?.medium || '/assets/img/unknown_car.jpg'} 2x`"
-			v-bind:src="imgUrl?.small || '/assets/img/unknown_car.jpg'"
+			v-bind:srcset="`${imgUrl?.small || `${basePath}/assets/img/unknown_car.jpg`}, ${
+				imgUrl?.medium || `${basePath}/assets/img/unknown_car.jpg`
+			} 2x`"
+			v-bind:src="imgUrl?.small || `${basePath}/assets/img/unknown_car.jpg`"
 			v-bind:alt="altText || ''"
 			class="car-img" />
 	</a>
